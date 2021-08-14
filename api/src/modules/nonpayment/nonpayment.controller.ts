be0c412c97be4a057bde8c71f068fec9c7a3b337
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-14 17:25:28 -0300

    feat(api/nonpayment): edr to find one nonpayment

--- api/src/modules/nonpayment/nonpayment.controller.ts
+++ api/src/modules/nonpayment/nonpayment.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateNonPaymentDto } from './dto';
 import { NonPaymentService } from './nonpayment.service';
 
 @Controller('nonpayments')
 export class NonPaymentController {
   constructor(private readonly nonPaymentService: NonPaymentService) {}
+
+  @Get('/:id')
+  async findOne(@Param('id') id: number) {
+    return this.nonPaymentService.findOne({ id });
+  }
 
   @Post()
   async create(@Body() args: CreateNonPaymentDto) {
--- api/src/modules/nonpayment/nonpayment.service.ts
+++ api/src/modules/nonpayment/nonpayment.service.ts
@@ -1,8 +1,15 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Where } from 'src/utils';
 import { Repository } from 'typeorm';
 import { TitleService, UserService } from '..';
 import { CreateNonPaymentDto } from './dto';
+import { NONPAYMENTNOTFOUND } from './nonpayment.const';
 import { NonPaymentORM } from './nonpayment.entity';
 
 @Injectable()
@@ -15,6 +22,13 @@ export class NonPaymentService {
     @Inject(forwardRef(() => TitleService))
     private readonly titleService: TitleService,
   ) {}
+
+  async findOne(where?: Where<NonPaymentORM>) {
+    const nonPayment = await this.nonPaymentRepository.findOne({ where });
+    if (!nonPayment) throw new NotFoundException(NONPAYMENTNOTFOUND);
+
+    return nonPayment;
+  }
 
   async create({ user_id, title_id, ...args }: CreateNonPaymentDto) {
     const user = await this.userService.findOne({ id: user_id });
