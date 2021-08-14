789e620f20c077b85fb4345ab894a83921e6266b
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-14 15:41:32 -0300

    refactor(api): add index to modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules';
 
 config();
 
