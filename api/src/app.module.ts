df3da3266ed5ae2942a1a5db6ecdf6638f20c404
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-14 14:45:32 -0300

    feat(api): add typeorm and remove unused files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: String(DB_HOST),
+      port: Number(DB_PORT),
+      username: String(DB_USER),
+      password: String(DB_PASS),
+      database: String(DB_NAME),
+      entities: [__dirname + '../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      dropSchema: false,
+      autoLoadEntities: true,
+      keepConnectionAlive: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
