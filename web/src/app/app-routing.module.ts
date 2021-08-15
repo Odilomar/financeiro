af3a858c5befbbefd5c799c4f99dc773cc074cca
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-15 13:03:42 -0300

    feat(web): add boostrap and list-user component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUserComponent } from './pages/user/list-user/list-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListUserComponent },
+  // { path: '', redirectTo: '/user', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ web/src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserModule } from './user/user.module';
+
+@NgModule({
+  declarations: [],
+  imports: [UserModule, CommonModule],
+})
+export class PagesModule {}
